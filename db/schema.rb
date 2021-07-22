4bac9f65adc52ad6f5a5ac9e8229c83f92d299ff
petumazo <davidalvarezmazzo@gmail.com>
2021-07-22 15:55:01 -0500

    Create group model and views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_22_185534) do
+ActiveRecord::Schema.define(version: 2021_07_22_203522) do
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
