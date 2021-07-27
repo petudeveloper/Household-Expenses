a68cf1260753390ab0187ae7801f918524e16c32
petumazo <davidalvarezmazzo@gmail.com>
2021-07-27 11:38:46 -0500

    Add group tests

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,6 @@ class Group < ApplicationRecord
   belongs_to :user
   has_many :group_expenses
   has_many :expenses, through: :group_expenses
+
+  validates :name, uniqueness: true, presence: true, allow_blank: false
 end
