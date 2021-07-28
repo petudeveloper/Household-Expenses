9da3a639ae11989f19529ad6653b294ebb9487cb
petumazo <davidalvarezmazzo@gmail.com>
2021-07-28 13:25:41 -0500

    Organize groups to display in alphabetical order.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,7 +4,7 @@ class GroupsController < ApplicationController
 
   # GET /groups or /groups.json
   def index
-    @groups = current_user.groups
+    @groups = current_user.groups.order(:name)
   end
 
   # GET /groups/1 or /groups/1.json
